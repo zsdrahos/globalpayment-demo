6fc088d5c126e9bf9a2d4a1132a881828747c702
Zsolt Drahos <zsdrahos@gmail.com>
2026-02-21 15:02:16 +0100

    spring boot initial

---
+++ src/main/java/com/example/globalpayment/GlobalpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalpaymentApplication.class, args);
+	}
+
+}
